10ac9928153173a96fb449fe3eef08a3f38cf262
Getu Taddesse <Getu_Taddesse@mckinsey.com>
2023-06-01 10:08:53 -0400

    fix: update description part in Education page

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -18,26 +18,27 @@ const Education = () => {
                 <ResumeLists 
                     title="Full Stack Software Engineer"
                     subtitle="Multiverse (2022 - 2023)"
-                    des="Secondary Education or post-primary education covers two phases on the International Standard 
-                    Classification of Education scale."
+                    des="Completed a rigorous training program on full-stack web development, with expertise in Test Driven Development, 
+                        CSS, HTML, Bootstrap, Express, Node, React, Redux, JavaScript, Postman API, Docker, MySQL, PostgreSQL and REST APIs.
+                        I have gained valuable experience building both frontend and backend application independently and in collaboration with cross-functional teams, 
+                        managing deadlines and deliverables, and delivering high-quality, scalable software solutions."
                 />
                 <ResumeLists 
                     title="Java Full Stack Developerr"
                     subtitle="Atlanta Technical College (03/2022)"
-                    des="Secondary Education or post-primary education covers two phases on the International Standard 
-                    Classification of Education scale."
+                    des="Participating in a Full Stack Java Developer program through Generation USA spending 560 hours learning technologies 
+                        to deploy full stack web applications using Git, Javascript, Java, HTML5, CSS3, SQL, and Bootstrap.Participating in a Full Stack Java Developer program through Generation USA spending over 460+ hours learning technologies to deploy full stack web applications using Git, Javascript, Java, HTML5, CSS3, SQL, and Bootstrap."
                 />
                 <ResumeLists 
                     title="Web Developer"
                     subtitle="Delgado Community College (09/2021)"
-                    des="Secondary Education or post-primary education covers two phases on the International Standard 
-                    Classification of Education scale."
+                    des="Participating in a Web Developer program through Generation USA spending over 400+ hours learning technologies 
+                    to deploy web applications using Git, Javascript, HTML5, CSS3 and Bootstrap."
                 />
                 <ResumeLists 
                     title="Master of Science GIS And Remote Sensing"
                     subtitle="Addis Ababa University (03/2009)"
-                    des="Secondary Education or post-primary education covers two phases on the International Standard 
-                    Classification of Education scale."
+                    des="Spatial database management and programming, Enviromental Risk Assessement, Spatial and Temporal Analysis."
                 />
             </div>
         </div>
@@ -51,19 +52,19 @@ const Education = () => {
             <ResumeLists 
                 title="Software Engineer"
                 subtitle="McKinsey & Company (2022 - 2023)"
-                des="•	Participate in designing, development, and implementation of highly scalable, high-volume software systems and components, 
+                des="Participate in designing, development, and implementation of highly scalable, high-volume software systems and components, 
                     both in frontend and backend development of client-facing web applications."
             />
             <ResumeLists 
                 title="Advisor/Geospatial Analyst"
                 subtitle="INSA (2009 - 2020)"
-                des="•	Collaborated with team members to build knowledge and skill on relational database, spatial analysis, 
+                des="Collaborated with team members to build knowledge and skill on relational database, spatial analysis, 
                     mapping as well as adapted and implemented an international standard and procedure. ."
             />
             <ResumeLists 
                 title="Researcher"
                 subtitle="EIAR (2020)"
-                des="Agricultural researcher, specifical involved in climate and Agicultural Risk Analysis Projects."
+                des="Agricultural researcher, saptial and temporal analysis, Agro-climate analysis, specifical involved in climate and Agicultural Risk Analysis Projects."
             />
             <ResumeLists 
                 title="Researcher"
