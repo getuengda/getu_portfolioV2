c234bb3f7a3567efe18bd388c3d6ac3028140f5f
Getu Taddesse <Getu_Taddesse@mckinsey.com>
2023-06-05 02:03:53 -0400

    update resume download button style

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -78,7 +78,7 @@ const LeftBanner = () => {
                 My Resume
                 </h2>
                 <div className='flex gap-4'>
-                    <button class="bg-black hover:bg-black text-white font-bold py-2 px-4 rounded-full">
+                    <button class="bannerIconDownload">
                     {<a href="https://drive.google.com/file/d/1yn487f_-Qfub5tStUUwyIzKtz5sd2lE1/view?usp=sharing" target='_blank' rel="noreferrer">Download Resume</a>}
                     </button>
                 </div>
